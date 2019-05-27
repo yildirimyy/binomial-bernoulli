8ac376bcd9fd26972c109cc2b615b2289c33fc55
Yagmur Yildirim <yildirm.yy@gmail.com>
2019-05-27 09:59:18 +0300

    code

--- bbs.py
+++ bbs.py
@@ -1,4 +1,12 @@
 # Binomial Dist
+#para atma problemi:
+# p = olasilik = 0.5
+# n = deneyin gerceklestirilme sayisi
+# tura = p
+# yazi = 1-p
+
+'''para 6 kere atiliyorsa 3 tura cikmasi maksimum olasilik,
+1 tura 5 yazi cikmasi minimum olasilik'''
 
 from scipy.stats import binom
 import matplotlib.pyplot as plt
